96ac3d2ae2062c2f72cf7b050598a6b7ab5bcbab
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2019-10-04 15:51:03 -0300

    first commit

---
+++ BinaryTree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Eduardo Ribeiro da Silva on 04/10/19.
+//
+
+#include "BinaryTree.h"
---
+++ Node.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Eduardo Ribeiro da Silva on 04/10/19.
+//
+
+#include "Node.h"
