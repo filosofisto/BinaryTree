9eac70ed779e0cddddbf1f2cf7b1ad77ad1c5db5
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2019-10-10 09:33:32 -0300

    Methods: add, empty and size

--- BinaryTree.h
+++ BinaryTree.h
@@ -5,23 +5,32 @@
 #ifndef BINARYTREE_BINARYTREE_H
 #define BINARYTREE_BINARYTREE_H
 
+#include <iostream>
 #include "Node.h"
+
+using namespace std;
 
 template <typename T>
 class BinaryTree {
 public:
-    explicit BinaryTree(shared_ptr<Node<T>> root);
+    explicit BinaryTree();
     virtual ~BinaryTree();
 
     bool find(const T &data) const;
+    BinaryTree<T>* add(const T &data);
+    bool empty() const;
+    size_t size() const;
 private:
     bool find(shared_ptr<Node<T>> node, const T &data) const;
+    void add(shared_ptr<Node<T>> node, const T&data);
 
     shared_ptr<Node<T>> root;
+    size_t _size;
 };
 
 template<typename T>
-BinaryTree<T>::BinaryTree(shared_ptr<Node<T>> root): root(root) {
+BinaryTree<T>::BinaryTree(): _size(0)
+{
 
 }
 
@@ -30,12 +39,14 @@ BinaryTree<T>::~BinaryTree() = default;
 
 
 template<typename T>
-bool BinaryTree<T>::find(const T &data) const {
+bool BinaryTree<T>::find(const T &data) const
+{
     return find(root, data);
 }
 
 template<typename T>
-bool BinaryTree<T>::find(shared_ptr<Node<T>> node, const T &data) const {
+bool BinaryTree<T>::find(shared_ptr<Node<T>> node, const T &data) const
+{
     if (node == nullptr) {
         //cout << "x" << endl;
         return false;
@@ -53,6 +64,51 @@ bool BinaryTree<T>::find(shared_ptr<Node<T>> node, const T &data) const {
 
     //cout << "-->" << endl;
     return find(node->right(), data);
+}
+
+template<typename T>
+BinaryTree<T>* BinaryTree<T>::add(const T &data)
+{
+    if (root == nullptr) {
+        root = make_shared<Node<T>>(data);
+        _size++;
+    } else {
+        add(root, data);
+    }
+
+    return this;
+}
+
+template<typename T>
+void BinaryTree<T>::add(shared_ptr<Node<T>> node, const T &data)
+{
+    if (data >= node->getData()) {
+        if (node->right() == nullptr) {
+            shared_ptr<Node<T>> newNode = make_shared<Node<T>>(data);
+            node->setRight(newNode);
+            _size++;
+        } else {
+            add(node->right(), data);
+        }
+    } else {
+        if (node->left() == nullptr) {
+            shared_ptr<Node<T>> newNode = make_shared<Node<T>>(data);
+            node->setLeft(newNode);
+            _size++;
+        } else {
+            add(node->left(), data);
+        }
+    }
+}
+
+template<typename T>
+bool BinaryTree<T>::empty() const {
+    return _size == 0;
+}
+
+template<typename T>
+size_t BinaryTree<T>::size() const {
+    return _size;
 }
 
 #endif //BINARYTREE_BINARYTREE_H
--- main.cpp
+++ main.cpp
@@ -14,55 +14,33 @@ int main() {
     cout << "         2       4       13" << endl;
     cout << "                     9" << endl;
 
-    // left path
-    auto node15 = make_shared<Node<int>>(Node<int>(15)); //root
-    auto node6 = make_shared<Node<int>>(Node<int>(6));
-    auto node7 = make_shared<Node<int>>(Node<int>(7));
-    auto node13 = make_shared<Node<int>>(Node<int>(13));
-    auto node9 = make_shared<Node<int>>(Node<int>(9));
-    auto node3 = make_shared<Node<int>>(Node<int>(3));
-    auto node2 = make_shared<Node<int>>(Node<int>(2));
-    auto node4 = make_shared<Node<int>>(Node<int>(4));
-    node15->setLeft(node6);
-    node6->setRight(node7);
-    node7->setRight(node13);
-    node13->setLeft(node9);
-    node6->setLeft(node3);
-    node3->setRight(node4);
-    node3->setLeft(node2);
+    unique_ptr<BinaryTree<int>> tree = make_unique<BinaryTree<int>>();
+    tree
+        ->add(15)
+        ->add(6)
+        ->add(7)
+        ->add(13)
+        ->add(9)
+        ->add(3)
+        ->add(2)
+        ->add(4)
+        ->add(18)
+        ->add(17)
+        ->add(20)
+        ;
 
-    //right path
-    auto node18 = make_shared<Node<int>>(Node<int>(18));
-    auto node17 = make_shared<Node<int>>(Node<int>(17));
-    auto node20 = make_shared<Node<int>>(Node<int>(20));
-    node15->setRight(node18);
-    node18->setLeft(node17);
-    node18->setRight(node20);
-
-    BinaryTree<int> tree = BinaryTree<int>(node15);
-
+    cout << "BinaryTree size: " << tree->size() << endl;
     int value;
 
-    cout << "Procurar por: ";
+    cout << "Desire value: ";
     cin >> value;
 
-    if (tree.find(value)) {
+    if (tree->find(value)) {
         cout << "Found!" << endl;
     } else {
         cout << "Not Found" << endl;
     }
 
-    /*delete(node15);
-    delete(node6);
-    delete(node7);
-    delete(node13);
-    delete(node9);
-    delete(node3);
-    delete(node2);
-    delete(node4);
-    delete(node18);
-    delete(node17);
-    delete(node20);*/
 
     return EXIT_SUCCESS;
 }
